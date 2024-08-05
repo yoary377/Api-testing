5e50728d590d15dc273829f5cc59f16c5138be6d
Ivan Kolesnikov <ivan_kolesnikov1@epam.com>
2024-08-05 13:50:25 +0200

    Added description

--- src/test/java/test_api/JsonTest.java
+++ src/test/java/test_api/JsonTest.java
@@ -3,6 +3,7 @@ package test_api;
 import api.PeopleApi;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
 public class JsonTest {
@@ -10,6 +11,7 @@ public class JsonTest {
     private final PeopleApi peopleApi = new PeopleApi();
 
     @Test
+    @Description("Verify json schema of /people API")
     public void jsonSchemaTest() {
         Response response = peopleApi.searchPeopleJson();
         response.then().
--- src/test/java/test_api/PeopleTests.java
+++ src/test/java/test_api/PeopleTests.java
@@ -4,6 +4,7 @@ import api.FilmsApi;
 import api.PeopleApi;
 import api.responses.PeopleSearchResponse;
 import api.responses.Person;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class PeopleTests {
     private final FilmsApi filmsApi = new FilmsApi();
 
     @Test
+    @Description("Check that Darth Vader can be searched")
     public void testSearchPersonByName() {
 
         String name = "Vader";
@@ -28,7 +30,21 @@ public class PeopleTests {
     }
 
     @Test
+    @Description("Find the film with fewer planets where Darth Vader starred")
     public void testSearchFewestPlanetsFilm() {
+        String name = "Vader";
+        PeopleSearchResponse peopleSearchResponse = peopleApi.searchPersonByName(name);
+        Person person = peopleSearchResponse.getResults().get(0);
+        List<String> films = person.getFilms();
+
+        String filmWithFewestPlanets = filmsApi.findFilmWithFewestPlanets(films);
+        assertThat(filmWithFewestPlanets, notNullValue());
+
+    }
+
+    @Test
+    @Description("Check that the Darth Vader's starship was in the film with fewest planets")
+    public void testSearchStarshipInFilm() {
         String name = "Vader";
         PeopleSearchResponse peopleSearchResponse = peopleApi.searchPersonByName(name);
         Person person = peopleSearchResponse.getResults().get(0);
@@ -42,10 +58,10 @@ public class PeopleTests {
 
         assertThat(starship,
                 in(filmsApi.searchFilm(filmWithFewestPlanets).getStarships()));
-
     }
 
     @Test
+    @Description("Get oldest person that starred in any SW film")
     public void getOldest() {
         Person oldest = peopleApi.getOldestCharacter();
         if (oldest != null) {
